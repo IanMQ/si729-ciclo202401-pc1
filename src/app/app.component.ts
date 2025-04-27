0c9df742318a85d0776b2cab319ab6a5fa8f2bc0
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-04-26 19:19:42 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'si729-ciclo202401-pc1';
+}
